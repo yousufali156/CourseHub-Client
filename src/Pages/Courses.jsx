484b0dfaf3c87a36a5cb761d145730a8bd50c136
Md. Yousuf Ali <yousufwebwork@gmail.com>
2025-06-17 22:08:14 +0600

    Update Courses.jsx

--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'; // ✅ Fix for react-router-dom
 import axios from 'axios';
+import helmet from 'helmet';
+
 
 const Courses = () => {
   const [courses, setCourses] = useState([]);
